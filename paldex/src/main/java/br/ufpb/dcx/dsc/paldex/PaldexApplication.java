8196c007c288e190149fc76c88ffc3a1281a0653
Lucas <lucas.bq31@outlok.com>
2024-10-15 16:34:28 -0300

    initial commit

---
+++ paldex/src/main/java/br/ufpb/dcx/dsc/paldex/PaldexApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.dsc.paldex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaldexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaldexApplication.class, args);
+    }
+
+}
---
+++ paldex/src/test/java/br/ufpb/dcx/dsc/paldex/PaldexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.dsc.paldex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaldexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
